cb096c9510fe9c198e6d1e62266526608bb36582
cyr580 <58054530@qq.com>
2021-03-05 13:30:53 +0800

    a

--- depictions/index.js
+++ depictions/index.js
@@ -166,9 +166,9 @@ $(document).ready(function () {
 
         var links = data.links;
         var extra = {
-            "<img class=\"icon\" src=\"icons/twitter.png\"><span>Find me on Twitter (@ev_ynw)</span>": "https://twitter.com/ev_ynw",
-            "<img class=\"icon\" src=\"icons/email.png\"><span>Send me an email</span>": "mailto:ev.ynam.w@gmail.com",
-            "<img class=\"icon\" src=\"icons/like.png\"><span>Buy me a &#x1F37A; via <span style=\"font-style:italic;font-weight:bold;\"><span style=\"color:#253b80;\">Pay</span><span style=\"color:#419bd7;\">Pal</strong></span></span>": "https://paypal.me/evynw"
+            "<img class=\"icon\" src=\"icons/twitter.png\"><span>关注我的推特</span>": "https://twitter.com/CNCYRCOM",
+            "<img class=\"icon\" src=\"icons/email.png\"><span>发送邮件</span>": "cyr580@qq.com",
+            "<img class=\"icon\" src=\"icons/like.png\"><span>暂未开通 &#x1F37A;  <span style=\"font-style:italic;font-weight:bold;\"><span style=\"color:#253b80;\">Pay</span><span style=\"color:#419bd7;\">Pal</strong></span></span>": "https://"
         };
         $.extend(links, extra);
         var lKeys = Object.keys(links);
